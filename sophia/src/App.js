c50de8333192bd8ab79c197be1a6fc8a28b8c707
Aunik Ahmed <aunik8013@gmail.com>
2019-02-24 22:02:07 -0500

    Finished Help Page

--- sophia/src/App.js
+++ sophia/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import Home from "./components/Home";
+import Help from "./components/Help";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Home />
+        {/* <Help /> */}
       </div>
     );
   }
--- sophia/src/components/Help.js
+++ sophia/src/components/Help.js
@@ -1,7 +1,70 @@
-import React from "react";
+import React, { Component } from "react";
+import "./Help.css";
+import { Navbar, CardDeck, Card } from "react-bootstrap";
 
-const Help = () => {
-  return <h1>Help Page</h1>;
-};
+class Help extends Component {
+  render() {
+    return (
+      <div className="help">
+        <Navbar fixed="top" variant="dark" className="navbar">
+          <div className="logo">SOPHIA'S STORYBOOK</div>
+        </Navbar>
+
+        {/* 3 helper Cards */}
+
+        <CardDeck className="helper-cards">
+          {/* 1st helper Card */}
+
+          <Card className="helper">
+            <Card.Header className="helper-header helper-header-1">
+              START STORY
+            </Card.Header>
+            <Card.Body className="helper-1">
+              <Card.Text>PRESS THIS BUTTON TO START THE STORYBOOK</Card.Text>
+            </Card.Body>
+          </Card>
+
+          {/* 2nd helper Card */}
+
+          <Card className="helper">
+            <Card.Header className="helper-header helper-header-2">
+              SKIP PAGES
+            </Card.Header>
+            <Card.Body className="helper-2">
+              <Card.Text>PRESS THIS BUTTON TO SKIP TO ACTIVITIES</Card.Text>
+            </Card.Body>
+          </Card>
+
+          {/* 3rd helper Card */}
+
+          <Card className="helper">
+            <Card.Header className="helper-header helper-header-3">
+              ACTIVITIES
+            </Card.Header>
+            <Card.Body className="helper-3">
+              <Card.Text>
+                YOU NEED TO COMPLETE CERTAIN ACTIVITIES THROUGHOUT THE BOOK TO
+                PROGRESS THE STORY
+              </Card.Text>
+            </Card.Body>
+          </Card>
+
+          {/* 4th helper Card */}
+
+          <Card className="helper">
+            <Card.Header className="helper-header helper-header-4">
+              ARROWS
+            </Card.Header>
+            <Card.Body className="helper-4">
+              <Card.Text>
+                THESE BUTTONS HELP YOU NAVIGATE TO THE NEXT AND PREVIOUS PAGES
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </CardDeck>
+      </div>
+    );
+  }
+}
 
 export default Help;
