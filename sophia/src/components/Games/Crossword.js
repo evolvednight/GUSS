4cc7789c69b2c5fe41d4f32814785bbe1e83c198
evolvednight <edwinh13148@gmail.com>
2019-03-04 04:48:40 -0500

    Create game folder and put instructions on creating games

---
+++ sophia/src/components/Games/Crossword.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import { Button } from "react-bootstrap";
+
+class Crossword extends Component {
+
+    render() {
+        return (
+            <div>
+                <Button onClick="none">Next</Button>
+            </div>
+        )
+    }
+
+}
+
+export default Crossword;
