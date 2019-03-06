64afbcb250cf27ad356206726979bf863faee3ec
evolvednight <edwinh13148@gmail.com>
2019-03-06 00:46:41 -0500

    adding second unit test - story book pages

---
+++ sophia/src/tests/Pages.test.js
@@ -0,0 +1,79 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import Page1 from "../components/Pages/Page_1";
+import Page2 from "../components/Pages/Page_2";
+import Page3 from "../components/Pages/Page_3";
+import Page4 from "../components/Pages/Page_4";
+import Page5 from "../components/Pages/Page_5";
+import Page6 from "../components/Pages/Page_6";
+import Page7 from "../components/Pages/Page_7";
+import Page8 from "../components/Pages/Page_8";
+import Page9 from "../components/Pages/Page_9";
+import Page10 from "../components/Pages/Page_10";
+import Page11 from "../components/Pages/Page_11";
+import Page12 from "../components/Pages/Page_12";
+import Page13 from "../components/Pages/Page_13";
+import Page14 from "../components/Pages/Page_14";
+import Page15 from "../components/Pages/Page_15";
+import Page16 from "../components/Pages/Page_16";
+import Page17 from "../components/Pages/Page_17";
+import Page18 from "../components/Pages/Page_18";
+import Page19 from "../components/Pages/Page_19";
+import Page20 from "../components/Pages/Page_20";
+import Page21 from "../components/Pages/Page_21";
+import Page22 from "../components/Pages/Page_22";
+import Page23 from "../components/Pages/Page_23";
+import Page24 from "../components/Pages/Page_24";
+import Page25 from "../components/Pages/Page_25";
+import Page26 from "../components/Pages/Page_26";
+import Page27 from "../components/Pages/Page_27";
+import Page28 from "../components/Pages/Page_28";
+import Page29 from "../components/Pages/Page_29";
+import Page30 from "../components/Pages/Page_30";
+import Page31 from "../components/Pages/Page_31";
+import Page32 from "../components/Pages/Page_32";
+import Page33 from "../components/Pages/Page_33";
+
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(
+    <BrowserRouter>
+        <Page1 />
+        <Page2 />
+        <Page3 />
+        <Page4 />
+        <Page5 />
+        <Page6 />
+        <Page7 />
+        <Page8 />
+        <Page9 />
+        <Page10 />
+        <Page11 />
+        <Page12 />
+        <Page13 />
+        <Page14 />
+        <Page15 />
+        <Page16 />
+        <Page17 />
+        <Page18 />
+        <Page19 />
+        <Page20 />
+        <Page21 />
+        <Page22 />
+        <Page23 />
+        <Page24 />
+        <Page25 />
+        <Page26 />
+        <Page27 />
+        <Page28 />
+        <Page29 />
+        <Page30 />
+        <Page31 />
+        <Page32 />
+        <Page33 />
+    </BrowserRouter>,
+    div
+  );
+  ReactDOM.unmountComponentAtNode(div);
+});
