704d32ae2d8a7de610528d6c3e6f667f1eb58ab3
jdong7 <jdong7@buffalo.edu>
2019-05-06 02:43:50 -0400

    hyperlinked a google survey

--- sophia/src/components/Pages/Page_8.js
+++ sophia/src/components/Pages/Page_8.js
@@ -1,13 +1,9 @@
 import React, { Component } from "react";
 import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
-<<<<<<< HEAD
 import WordSearch from "../Games/WordSearch";
-
-=======
 import press from './Sounds/press.mp3';
 import sounds from './Sounds/page8.m4a';
->>>>>>> Sound_mergeFix
 class Page8 extends Component {
   constructor(props) {
     super(props);
@@ -77,13 +73,10 @@ class Page8 extends Component {
         <div className="text">
           Can you help Sofia find all the words she just learned about?
         </div>
-<<<<<<< HEAD
         <WordSearch />
         <div className="text" />
-=======
 
         <div className="text">Game</div>
->>>>>>> Sound_mergeFix
         <Link to="/page7" style={{ textDecoration: "none" }}>
           <Button
             onKeyDown={this.handleKeyPress}
@@ -93,12 +86,9 @@ class Page8 extends Component {
           >
             Prev Page
           </Button>
-<<<<<<< HEAD
         </Link>
         <Link to="/page7" style={{ textDecoration: "none" }}>
           <Button className="leftBtn">Prev Page</Button>
-=======
->>>>>>> Sound_mergeFix
         </Link>
         <Link to="/page9" style={{ textDecoration: "none" }}>
           <Button
