9608c84f7f9c95b1959647dfc0bdd2c451de1e78
evolvednight <edwinh13148@gmail.com>
2019-05-07 18:34:26 -0400

    add additional states for crosswords

--- sophia/src/components/Games/Crossword.js
+++ sophia/src/components/Games/Crossword.js
@@ -22,7 +22,86 @@ class Crossword extends Component {
         answer19: '',
         answer110: '',
         answer111: '',
-
+        answer21: '',
+        answer22: '',
+        answer23: '',
+        answer24: '',
+        answer31: '',
+        answer32: '',
+        answer33: '',
+        answer34: '',
+        answer35: '',
+        answer36: '',
+        answer37: '',
+        answer38: '',
+        answer39: '',
+        answer310: '',
+        answer311: '',
+        answer41: '',
+        answer42: '',
+        answer43: '',
+        answer44: '',
+        answer45: '',
+        answer46: '',
+        answer47: '',
+        answer51: '',
+        answer52: '',
+        answer53: '',
+        answer54: '',
+        answer55: '',
+        answer56: '',
+        answer61: '',
+        answer62: '',
+        answer63: '',
+        answer64: '',
+        answer65: '',
+        answer66: '',
+        answer67: '',
+        answer68: '',
+        answer69: '',
+        answer610: '',
+        answer71: '',
+        answer72: '',
+        answer73: '',
+        answer74: '',
+        answer75: '',
+        answer76: '',
+        answer77: '',
+        answer78: '',
+        answer79: '',
+        answer710: '',
+        answer711: '',
+        answer712: '',
+        answer713: '',
+        answer714: '',
+        answer81: '',
+        answer82: '',
+        answer83: '',
+        answer84: '',
+        answer85: '',
+        answer86: '',
+        answer87: '',
+        answer88: '',
+        answer89: '',
+        answer810: '',
+        answer91: '',
+        answer92: '',
+        answer93: '',
+        answer94: '',
+        answer95: '',
+        answer101: '',
+        answer102: '',
+        answer103: '',
+        answer104: '',
+        answer105: '',
+        answer106: '',
+        answer107: '',
+        answer111: '',
+        answer112: '',
+        answer113: '',
+        answer114: '',
+        answer115: '',
+        answer116: '',
       };
       this.handle_value_1_input = this.handle_value_1_input.bind(this);
       this.handle_value_2_input = this.handle_value_2_input.bind(this);
@@ -51,13 +130,13 @@ class Crossword extends Component {
               <td className="cell cell1" style={tdStyles}></td>
               <td className="cell cell2" style={tdStyles}></td>
               <td className="cell cell3" style={tdStyles}></td>
-              <td className="cell cell4"><input type="text" ref="answer1807" maxLength="1" onChange={(event) => this.handle_value_1_input(event)} /></td>
+              <td className="cell cell4" style={tdStyles}></td>
               <td className="cell cell5" style={tdStyles}></td>
               <td className="cell cell6" style={tdStyles}></td>
               <td className="cell cell7" style={tdStyles}></td>
               <td className="cell cell8" style={tdStyles}></td>
               <td className="cell cell9" style={tdStyles}></td>
-              <td className="cell cell10" style={tdStyles}></td>
+              <td className="cell cell10"><input type="text" ref="answer1807" maxLength="1" onChange={(event) => this.handle_value_1_input(event)} /></td>
               <td className="cell cell11" style={tdStyles}></td>
               <td className="cell cell12" style={tdStyles}></td>
               <td className="cell cell13" style={tdStyles}></td>
