cbab5443bc4df0f91f409bb84cb89b9b9122cc40
Aunik Ahmed <aunik8013@gmail.com>
2019-05-08 11:24:26 -0400

    Added wrong tests to the last commit, so recommitting now with the right tests

--- sophia/src/tests/WordSearch.test.js
+++ sophia/src/tests/WordSearch.test.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import Help from "../components/Help";
+import WordSearch from "../components/Games/WordSearch";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(
     <BrowserRouter>
-      <Help />
+      <WordSearch />
     </BrowserRouter>,
     div
   );
