042a4d2cfa0122394eb869b07b8682c7a4e8c6d6
evolvednight <edwinh13148@gmail.com>
2019-05-08 14:09:25 -0400

    pushing crosswords for checking

--- sophia/src/components/Games/Crossword.js
+++ sophia/src/components/Games/Crossword.js
@@ -172,9 +172,9 @@ class Crossword extends Component {
       answer2[parseInt(answerNum[1])] = changeValue; //execute the manipulations
       this.setState({answer2})
       if(newTwo === this.state.answer[1]) {
-        this.className.two.style="background:green";
+        this.refs.two.style="background:green";
       } else {
-        this.className.two.style="background:white";
+        this.refs.two.style="background:white";
       }
     }
     console.log({newOne});
